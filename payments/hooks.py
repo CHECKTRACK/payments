0e7156ccc77deead25de18df0e445670361aec1f
mihirpatel1510 <mihir.patel@team.satat.tech>
2025-10-14 13:02:20 +0530

    cancle after end of billing period

--- payments/hooks.py
+++ payments/hooks.py
@@ -108,12 +108,12 @@ override_doctype_class = {"Web Form": "payments.overrides.payment_webform.Paymen
 # 	}
 # }
 
-doc_events = {
-    "Subscription": {
-        "after_save": "payments.api.stripe_cancel_subscription",
-        "on_update": "payments.api.stripe_cancel_subscription",
-    }
-}
+# doc_events = {
+#     "Subscription": {
+#         "after_save": "payments.api.stripe_cancel_subscription",
+#         "on_update": "payments.api.stripe_cancel_subscription",
+#     }
+# }
 
 # Scheduled Tasks
 # ---------------
