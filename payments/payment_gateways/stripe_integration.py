5e448ef98516fb1a5ed5cf7c125075b0d8f190cb
mihirpatel1510 <mihir.patel@team.satat.tech>
2025-10-24 16:56:59 +0530

    add billing_mode flexible for yearly and monthly

--- payments/payment_gateways/stripe_integration.py
+++ payments/payment_gateways/stripe_integration.py
@@ -81,6 +81,7 @@ def create_subscription_on_stripe(stripe_settings):
 		subscription = stripe.Subscription.create(
 			customer=customer,
 			items=items,
+			billing_mode="flexible"
 		)
 
 		if subscription.status == "active":
