37f5f33f377a10334d4b224b3093546994ab3513
mihirpatel1510 <mihir.patel@team.satat.tech>
2025-10-24 17:39:33 +0530

    set api version

--- payments/payment_gateways/stripe_integration.py
+++ payments/payment_gateways/stripe_integration.py
@@ -10,42 +10,43 @@ from frappe.integrations.utils import create_request_log
 
 
 def create_stripe_subscription(gateway_controller, data):
-    stripe_settings = frappe.get_doc("Stripe Settings", gateway_controller)
-    stripe_settings.data = frappe._dict(data)
+	stripe_settings = frappe.get_doc("Stripe Settings", gateway_controller)
+	stripe_settings.data = frappe._dict(data)
 
-    stripe.api_key = stripe_settings.get_password(fieldname="secret_key", raise_exception=False)
-    stripe.default_http_client = stripe.http_client.RequestsClient()
+	stripe.api_key = stripe_settings.get_password(fieldname="secret_key", raise_exception=False)
+	stripe.default_http_client = stripe.http_client.RequestsClient()
+	stripe.api_version = "2025-06-30.basil"
 
-    try:
-        stripe_settings.integration_request = create_request_log(stripe_settings.data, "Host", "Stripe")
+	try:
+		stripe_settings.integration_request = create_request_log(stripe_settings.data, "Host", "Stripe")
 
-        # Fetch Subscription Plans from Subscription Doctype instead of Payment Plan
-        payment_request_doc = frappe.get_doc("Payment Request", stripe_settings.data.reference_docname)
+		# Fetch Subscription Plans from Subscription Doctype instead of Payment Plan
+		payment_request_doc = frappe.get_doc("Payment Request", stripe_settings.data.reference_docname)
 
-        if payment_request_doc.is_a_subscription:
-            # Pull all subscription plan details for this request
-            stripe_settings.payment_plans = frappe.get_all(
+		if payment_request_doc.is_a_subscription:
+			# Pull all subscription plan details for this request
+			stripe_settings.payment_plans = frappe.get_all(
 				"Subscription Plan Detail",
 				filters={"parent": payment_request_doc.name},
 				fields=["plan", "qty"]
-            )
-        else:
-            stripe_settings.payment_plans = []
+			)
+		else:
+			stripe_settings.payment_plans = []
 
-        return create_subscription_on_stripe(stripe_settings)
+		return create_subscription_on_stripe(stripe_settings)
 
-    except Exception:
-        stripe_settings.log_error("Unable to create Stripe subscription")
-        return {
-            "redirect_to": frappe.redirect_to_message(
-                _("Server Error"),
-                _(
-                    "It seems that there is an issue with the server's stripe configuration. "
-                    "In case of failure, the amount will get refunded to your account."
-                ),
-            ),
-            "status": 401,
-        }
+	except Exception:
+		stripe_settings.log_error("Unable to create Stripe subscription")
+		return {
+			"redirect_to": frappe.redirect_to_message(
+				_("Server Error"),
+				_(
+					"It seems that there is an issue with the server's stripe configuration. "
+					"In case of failure, the amount will get refunded to your account."
+				),
+			),
+			"status": 401,
+		}
 
 
 def create_subscription_on_stripe(stripe_settings):
