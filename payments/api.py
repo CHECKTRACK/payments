bc6594847d77266a7d76f68da3ffd43e92a4bc96
mihirpatel1510 <mihir.patel@team.satat.tech>
2025-10-26 21:35:41 +0530

    change done

--- payments/api.py
+++ payments/api.py
@@ -42,7 +42,7 @@ def stripe_cancel_subscription(subscription_id):
             # If current date < cancel_at_date, we must charge remaining months
             today = getdate(nowdate())
             if today < cancel_at_date:
-                remaining_months = (cancel_at_date.year - today.year) * 12 + (cancel_at_date.month - today.month)
+                remaining_months = (cancel_at_date.year - today.year) * 12 + (cancel_at_date.month - start_date.month)
 
                 if remaining_months > 0:
                     # Get Stripe Customer from ERPNext subscription
