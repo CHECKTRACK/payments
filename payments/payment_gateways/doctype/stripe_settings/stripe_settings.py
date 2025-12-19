a2b185c5d18a3dd6c91a073a72bfbb319d82f9d5
mihirpatel1510 <mihir.patel@team.satat.tech>
2025-12-19 17:34:04 +0530

    change timezone

--- payments/payment_gateways/doctype/stripe_settings/stripe_settings.py
+++ payments/payment_gateways/doctype/stripe_settings/stripe_settings.py
@@ -232,7 +232,7 @@ class StripeSettings(Document):
 				booking = frappe.get_doc("Booking", booking_id)
 
 				booking_start = get_datetime(booking.from_datetime)
-				current_time = now_datetime().astimezone(pytz.timezone("America/Los_Angeles"))
+				current_time = now_datetime().astimezone(pytz.UTC).astimezone(pytz.timezone("America/Los_Angeles"))
 				frappe.log_error("current_time",current_time)
 				frappe.log_error("booking_start",booking_start)
 
